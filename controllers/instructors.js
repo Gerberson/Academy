23a58f338790ff9ab02dff128b6d396f3c11d33c
Gerberson Dias <gerbersondias.312@gmail.com>
2020-07-14 11:36:59 -0300

    Trabalhando com data

--- controllers/instructors.js
+++ controllers/instructors.js
@@ -1,10 +1,23 @@
+const fs = require('fs')
+const data = require('../data.json')
+
 exports.post = (req, res) => {
     const keys = Object.keys(req.body)
 
     for(key of keys) {
         if (req.body[key] == "")
-            return res.send("Por favor, preencha todos os campos!")
+            return res.send("Todos os campos dever ser preenchidos.")
     }
 
-    return res.send(keys)
+    req.body.created_at = Date.now()
+    req.body.birth = Date.parse(req.body.birth)
+
+    data.instructors.push(req.body)
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
+        if (err)
+            return res.send('Write file error!')
+        
+        return res.redirect('/instructors')
+    })
 }
