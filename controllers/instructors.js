8e3f8a96e39cc1c782bd6498f0219a2a8effa158
Gerberson Dias <gerbersondias.312@gmail.com>
2020-07-15 18:00:57 -0300

    Implementando paginas de Criar Exibir e Editar Instructor

--- controllers/instructors.js
+++ controllers/instructors.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const data = require('../data.json')
+const { age, date } = require('../utils')
 
 exports.post = (req, res) => {
     const keys = Object.keys(req.body)
@@ -49,5 +50,62 @@ exports.show = (req, res) => {
     if (!foundIntructor)
         return res.send('Instructor not found!')
 
-    return res.send(foundIntructor)
+    const instructor = {
+        ...foundIntructor,
+        age: age(foundIntructor.birth),
+        services: foundIntructor.services.split(','),
+        created_at: new Intl.DateTimeFormat("pt-BR").format(foundIntructor.created_at)
+    }
+
+    return res.render('instructors/show', { instructor: instructor })
+}
+
+exports.edit = (req, res) => {
+    const { id } = req.params
+
+    const foundIntructor = data.instructors.find((instructor) => {
+        return instructor.id == id
+    })
+
+    if (!foundIntructor)
+        return res.send('Instructor not found!')
+
+    const instructor = {
+        ...foundIntructor,
+        birth: date(foundIntructor.birth)
+    }
+    
+    return res.render('instructors/edit', { instructor: instructor })
+}
+
+exports.put = (req, res) => {
+    const { id } = req.body
+
+    let index = 0
+    const foundIntructor = data.instructors.find((instructor, foundIndex) => {
+        if (instructor.id == id) {
+            index = foundIndex
+            return true
+        }
+    })
+
+    if (!foundIntructor)
+        return res.send('Instructor not found!')
+
+    const instructor = {
+        ...foundIntructor,
+        ...req.body,
+        birth: Date.parse(req.body.birth)
+    }
+
+    data.instructors[index] = instructor
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
+        if (err)
+            return res.send("Write error")
+        
+        return res.redirect(`/instructors/${id}`)
+    })
+    
+    return res.render('instructors/edit', { instructor: instructor })
 }
--- routes.js
+++ routes.js
@@ -16,7 +16,11 @@ routes.get('/instructors/create', (req, res) => {
 
 routes.get('/instructors/:id', instructors.show)
 
+routes.get('/instructors/:id/edit', instructors.edit)
+
 routes.post('/instructors', instructors.post)
+
+routes.put('/instructors', instructors.put)
 
 routes.get('/members', (req, res) => {
     return res.send('members')
