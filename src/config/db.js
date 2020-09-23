b800c40bd5efe79c2e4e3bf311a83f642a99859b
Gerberson Dias <gerbersondias.312@gmail.com>
2020-09-22 21:20:45 -0300

    Adicionando config db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,9 @@
 const { Pool } = require('pg')
 
 module.exports = new Pool({
-    user: "postgres",
-    password: "postgres",
-    host: "localhost",
+    user: "pglptrbegrwsfu",
+    password: "e4ea8fa7d5e68e99875080c96ac371a859d090618d4f4283eff7dabe939e6b57",
+    host: "ec2-52-73-199-211.compute-1.amazonaws.com",
     port: 5432,
-    database: "gymmanager"
+    database: "d948931onk50a8"
 })
